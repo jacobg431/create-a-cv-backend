ab7999503f5ac9ea5e8f149d973d1c8edca810eb
Jacob Gomez Hansen <jacobg431@gmail.com>
2024-08-24 13:43:57 +0200

    Added validation to SkillsSegment

--- src/main/java/com/cvbackend/springboot/maven/api/models/SkillsSegment.java
+++ src/main/java/com/cvbackend/springboot/maven/api/models/SkillsSegment.java
@@ -1,6 +1,9 @@
 package com.cvbackend.springboot.maven.api.models;
 
 import java.util.List;
+
+import org.javatuples.Pair;
+
 import lombok.Data;
 
 @Data
@@ -10,5 +13,43 @@ public class SkillsSegment {
     @Data
     public static class Skill {
         private String skill;
+        private int instanceIndex;
+
+        public void ValidateSkill(int instanceIndex) throws Exception {
+            this.instanceIndex = instanceIndex;
+            if (this.skill == null) {
+                throw new Exception("Skill not supplied for skill instance " + this.instanceIndex);
+            }
+        }
     }
+
+    private Boolean isValid;
+    private String validationErrorMsg;
+
+    public Pair<Boolean, String> Validate() {
+
+        this.isValid = true;
+        this.validationErrorMsg = "";
+
+        try {
+
+            ValidateSkills();
+            
+        } catch(Exception e) {
+            this.isValid = false;
+            this.validationErrorMsg = e.getMessage();
+        }
+
+        return new Pair<Boolean, String>(this.isValid, this.validationErrorMsg);
+
+    }
+
+    private void ValidateSkills() throws Exception {
+        int instanceIndex = 0;
+        for (Skill skill : skills) {
+            skill.ValidateSkill(instanceIndex);
+            instanceIndex++;
+        }
+    }
+
 }
